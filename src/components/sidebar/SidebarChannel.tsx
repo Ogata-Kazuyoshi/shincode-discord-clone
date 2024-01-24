92f509a3a3ff7cccd181e49cb23087913e5e240e
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-24 22:27:31 +0900

    initial commit

---
+++ src/components/sidebar/SidebarChannel.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SidebarChannel.scss';
+
+const SidebarChannel = () => {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannelHash"># Udemy</span>
+      </h4>
+      <h4>
+        <span className="sidebarChannelHash"># Udemy</span>
+      </h4>
+      <h4>
+        <span className="sidebarChannelHash"># Udemy</span>
+      </h4>
+      <h4>
+        <span className="sidebarChannelHash"># Udemy</span>
+      </h4>
+    </div>
+  );
+};
+
+export default SidebarChannel;
